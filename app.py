4abb796703ad1819cd3b3e0822802367f583cc78
starwoo1324 <starwoo1324@gmail.com>
2026-06-04 22:21:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import json
+import requests
 
 app = Flask(__name__)
 
@@ -13,3 +14,28 @@ def home():
 @app.route("/heroes")
 def heroes_api():
     return jsonify(heroes)
+
+@app.route("/blizzard-test")
+def blizzard_test():
+    try:
+        url = "https://overwatch.blizzard.com/ko-kr/rates?input=PC&map=all-maps&region=Asia&role=All&rq=1&tier=Master"
+
+        response = requests.get(
+            url,
+            headers={
+                "User-Agent": "Mozilla/5.0"
+            },
+            timeout=20
+        )
+
+        return jsonify({
+            "success": True,
+            "status": response.status_code,
+            "length": len(response.text)
+        })
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error": str(e)
+        })
